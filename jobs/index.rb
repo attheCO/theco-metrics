0e402f77fec40f0f8ba18b100effcc77758bd8de
Chance Smith <chancesmithb@gmail.com>
2017-10-09 11:15:51 -0500

    udpates dashboard for sept 2017 metrics

--- jobs/index.rb
+++ jobs/index.rb
@@ -1,4 +1,4 @@
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '1m' do
-  send_event('mentoring', {value:18})
+  send_event('dan', {value:0})
 end
